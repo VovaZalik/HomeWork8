70ef0c68f632734143b815fdceb174ae8b6d3dd1
VovaZ <zalikvova1998@gmail.com>
2023-05-30 13:25:39 +0300

    HH

---
+++ src/Basket.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Basket {
+    protected int[] prices;
+    protected String[] products;
+    protected int[] counts;
+    public Basket () {
+
+    }
+    public Basket(int[] prices, String[] products) {
+        this.prices = prices;
+        this.products = products;
+        this.counts = new int[products.length];
+    }
+
+    public void addToCart(int productNum, int amount) {
+        counts[productNum] += amount;
+    }
+    public void printCart() {
+        int sum = 0;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > 0) {
+                System.out.println(
+                        products[i] + " " +
+                                prices[i] + " руб за шт; " + " " + "Вы добавили в корзину: " +
+                                counts[i] + " шт; " + "В сумме выходит: " +
+                                (counts[i] * prices[i]));
+                sum += (counts[i] * prices[i]);
+            }
+        }
+        System.out.println("Итого : " + sum + " руб");
+    }
+    public void saveTxt(File textFile) throws FileNotFoundException {
+        try(PrintWriter printWriter = new PrintWriter(textFile)){
+            for (int price : prices) {
+                System.out.print(price + " ");
+            }
+            for (String product : products) {
+                System.out.print(product + " ");
+            }
+            for (int count : counts) {
+                System.out.print(count + " ");
+            }
+        }
+    }
+    public static Basket loadFromTxtFile(File textFile) {
+        Basket basket = new Basket();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))) {
+            String productsStr = bufferedReader.readLine();
+            String pricesStr = bufferedReader.readLine();
+            String countsStr = bufferedReader.readLine();
+
+            basket.products = productsStr.split(" ");
+            basket.prices = Arrays.stream(pricesStr.split(" "))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+            basket.counts = Arrays.stream(countsStr.split(" "))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return basket;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] products = {"Хлеб", "Соль", "Молоко"};
+        int[] prices = {30, 15, 65};
+        Basket basket = null;
+        Scanner s = new Scanner(System.in);
+        File basketFile = new File("basket.txt");
+
+        if (basketFile.exists()) {
+            basket = Basket.loadFromTxtFile(basketFile);
+        } else basket = new Basket(prices,products);
+
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + "." + " " + products[i] + " " + prices[i] + " " + "руб/шт;");
+        }
+
+        while (true) {
+            menu();
+            String input = s.nextLine();
+            if ("end".equals(input)) break;
+            String[] parts = input.split(" ");
+            int productNum = Integer.parseInt(parts[0]) - 1;
+            int amount = Integer.parseInt(parts[1]);
+            basket.addToCart(productNum,amount);
+            basket.saveTxt(basketFile);
+        }
+        basket.printCart();
+    }
+
+    private static void menu() {
+        System.out.println("Введите номер товара и его количество!");
+        System.out.println();
+        System.out.println("Чтобы зaвершить набор корзины - введите end");
+    }
+}
